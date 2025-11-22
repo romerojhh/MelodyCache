495483cb4040b69f43270ad0e716045ef949fa90
romero hutapea <61641738+romerojhh@users.noreply.github.com>
2025-11-22 13:16:08 +0700

    throw here all

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,4 +1,4 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/d1';
 import Database from 'better-sqlite3';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,18 +1,39 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
 
-export const user = sqliteTable('user', {
-	id: text('id').primaryKey(),
-	age: integer('age')
+export const users = sqliteTable('users', {
+	id: integer('id').primaryKey({ autoIncrement: true }),
+	username: text('username').unique(),
+	passwordHash: text('password_hash')
 });
 
-export const session = sqliteTable('session', {
-	id: text('id').primaryKey(),
+export const music_links = sqliteTable('music_links', {                                                     
+    id: integer('id').primaryKey({ autoIncrement: true }),                                                  
+    userId: text('user_id')                                                                                
+        .notNull()                                                                                          
+        .references(() => users.id),                                                                         
+    originalUrl: text('original_url').notNull(),                                                           
+    platform: text('platform').notNull(),                                                                   
+    title: text('title'),                                                                                   
+    artist: text('artist'),                                                                                 
+    genre: text('genre'),                                                                                   
+    thumbnailUrl: text('thumbnail_url'),                                                                   
+    createdAt: integer('created_at', { mode: 'timestamp' })
+		.notNull()
+		.default(sql`(unixepoch())`),                                                    
+});
+
+export const sessions = sqliteTable('session', {
+	id: integer('id').primaryKey({ autoIncrement: true }),
 	userId: text('user_id')
 		.notNull()
-		.references(() => user.id),
-	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
+		.references(() => users.id),
+	expiresAt: integer('expires_at', { mode: 'timestamp' })
+		.notNull()
 });
 
-export type Session = typeof session.$inferSelect;
+export type Sessions = typeof sessions.$inferSelect;
 
-export type User = typeof user.$inferSelect;
+export type Users = typeof users.$inferSelect;
+
+export type MusicLinks = typeof music_links.$inferSelect
